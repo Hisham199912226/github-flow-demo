1203bda2abd45394bd94f73c2a7bb52ecf73d213
mohammad14231 <hidene2004@gmail.com>
2023-01-14 15:35:16 +0300

    feature3 is done

--- src/main/java/feature3.java
+++ src/main/java/feature3.java
@@ -2,5 +2,6 @@ public class feature3 {
     public static void showFeature3(){
         System.out.println("Start working on feature3");
         System.out.println("I am still working on feature3");
+        System.out.println("feature3 is done");
     }
 }
